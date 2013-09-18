e604cf818339ab7d7c517302c1393938ac1732a2
alexander polzin <alexander@girls.io>
2013-09-18 17:32:37 +0200

    become seriously annoying after 15 minutes

--- data/fb-warning.js
+++ data/fb-warning.js
@@ -1,1 +1,13 @@
-document.body.style.border = '25px solid red';
+$('body').prepend('<div id="time-wasted-warning">00:10:32</div>');
+var timeWastedWarning = $('#time-wasted-warning');
+timeWastedWarning.css('background-color', 'red');
+timeWastedWarning.css('height', '50px');
+timeWastedWarning.css('text-align', 'center');
+timeWastedWarning.css('font-weight', 'bold');
+timeWastedWarning.css('font-family', 'helvetica');
+timeWastedWarning.css('font-size', '40px');
+timeWastedWarning.css('color', 'white');
+
+self.port.on('updateClock', function(updatedTime){
+  timeWastedWarning.html(updatedTime);
+});
